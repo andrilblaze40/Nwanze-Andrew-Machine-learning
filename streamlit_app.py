4369458207ad0f1d8f84ad247d6ffa8ccd0557c9
andrilblaze40 <andrilizu35@gmail.com>
2025-08-12 21:00:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 Machine Learning App')
 
 st.info('This is a Machine Learning App')
+
+df = pd.read_csv("https://raw.githubusercontent.com/andrilblaze40/Nwanze-Andrew-Machine-learning/refs/heads/master/.streamlit/cleaned_breast_cancer_data.csv")
+df
