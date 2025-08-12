35820e1ebb48fb0c3dabadd7d94aa7fecb18817a
andrilblaze40 <andrilizu35@gmail.com>
2025-08-12 21:51:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,16 @@ st.title('🎈 Machine Learning App')
 st.info('This is a Machine Learning App')
 with st.expander("Data"):
   st.write("**Raw Data**")
+  df = pd.read_csv("https://raw.githubusercontent.com/andrilblaze40/Nwanze-Andrew-Machine-learning/refs/heads/master/.streamlit/cleaned_breast_cancer_data.csv")
+  df
+
   st.write("**X**")
   X = df.drop("diagnosis", inplace=True)
   X
+  
   st.write("**y**")
   y = df.diagnosis
   y
 
 
 
-df = pd.read_csv("https://raw.githubusercontent.com/andrilblaze40/Nwanze-Andrew-Machine-learning/refs/heads/master/.streamlit/cleaned_breast_cancer_data.csv")
-df
