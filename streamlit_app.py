5bbc319812a29f93d5983feea469de603e1cfd4f
andrilblaze40 <andrilizu35@gmail.com>
2025-08-12 23:18:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ with st.sidebar:
           'smoothness_se': smoothness_se,
           'smoothness_mean': smoothness_mean}
   input_df = pd.DataFrame(data, index=[0])
-  input_predictors = pd.concat([input_df, X_raw], axis=0)
+  input_predictors = pd.concat([input_df, X], axis=0)
       
 
 						
