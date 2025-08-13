916e57a2288448e00d029a2b148c586cddda38a3
andrilblaze40 <andrilizu35@gmail.com>
2025-08-14 00:44:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,15 @@
-
+import streamlit as st
+import pandas as pd
+from sklearn.base import ClassifierMixin
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.model_selection import GridSearchCV, cross_val_score, train_test_split
+from sklearn.pipeline import make_pipeline
+import seaborn as sns
 
 
 
