453219482c88981a4e46e9e1bd2458253decd77c
andrilblaze40 <andrilizu35@gmail.com>
2025-08-16 17:25:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -175,8 +175,8 @@ with st.expander('Accuracy Scores'):
   acc_test
   
 if st.button("Predict"):
-    prediction = model.predict(input_df)[0]
-    prob = model.predict_proba(input_df)[0][1]
+    prediction = model.predict(X_test)[0]
+    prob = model.predict_proba(X_test)[0][1]
 
     st.markdown("## 🔍 Prediction Result:")
     if prediction == 1:
